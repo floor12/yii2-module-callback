9e574d1bc78037054688dae4eb71dd46fc56859a
floor12 <floor12@floor12.net>
2023-05-22 18:49:29 +0200

    NL lang added.

---
+++ src/messages/nl/callback.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: floor12
+ * Date: 2019-03-25
+ * Time: 19:46
+ */
+
+
+return [
+    'Time' => 'Zeit',
+    'Name' => 'Name',
+    'Phone number' => 'Telefonnummer',
+    'Callback requests' => 'Rückrufanfragen',
+    'Search' => 'Suche',
+    'Request deleted' => 'Anfrage gelöscht',
+    'Callback request' => 'Rückrufanfrage',
+    'Send' => 'Senden',
+    'Close' => 'Schließen',
+    'Thank you' => 'Danke',
+    'Fill your name and phone number and we will callback you as soon as possible.' =>
+        'Geben Sie Ihren Namen und Ihre Telefonnummer ein und wir rufen Sie so schnell wie möglich zurück.',
+    'We will contact you soon. This window will be closed in 5 sec.' => 'Wir werden uns bald bei Ihnen melden. Dieses Fenster wird in 5 Sekunden geschlossen.',
+    'Close now' => 'Jetzt schließen',
+    'New callback request from website:' => 'Neue Rückrufanfrage von der Website:',
+    'New callback request' => 'Neue Rückrufanfrage',
+    'Topic' => 'Thema',
+    'All topics' => 'Alle Themen',
+];
