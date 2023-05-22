2353c48c66b3a703071f4424d35425aa2f1abeaf
floor12 <floor12@floor12.net>
2023-05-22 02:09:27 +0200

    Tr lang added.

---
+++ src/messages/tr/callback.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: floor12
+ * Date: 2019-03-25
+ * Time: 19:46
+ */
+
+
+return [
+    'Time' => 'Zaman',
+    'Name' => 'İsim',
+    'Phone number' => 'Telefon numarası',
+    'Callback requests' => 'Geri arama talepleri',
+    'Search' => 'Ara',
+    'Request deleted' => 'Talep silindi',
+    'Callback request' => 'Geri arama talebi',
+    'Send' => 'Gönder',
+    'Close' => 'Kapat',
+    'Thank you' => 'Teşekkür ederiz',
+    'Fill your name and phone number and we will callback you as soon as possible.' =>
+        'Adınızı ve telefon numaranızı girin, en kısa sürede size geri döneceğiz.',
+    'We will contact you soon. This window will be closed in 5 sec.' => 'En kısa sürede sizinle iletişime geçeceğiz. Bu pencere 5 saniye içinde kapatılacak.',
+    'Close now' => 'Şimdi kapat',
+    'New callback request from website:' => 'Web sitesinden yeni bir geri arama talebi:',
+    'New callback request' => 'Yeni geri arama talebi',
+    'Topic' => 'Konu',
+    'All topics' => 'Tüm konular',
+];
