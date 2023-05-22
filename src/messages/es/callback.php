5fbfcf1a47acfd3450c2cd7c824811a1423d5a02
floor12 <floor12@floor12.net>
2023-05-22 18:47:38 +0200

    ES and DE localizions added.

---
+++ src/messages/es/callback.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: floor12
+ * Date: 2019-03-25
+ * Time: 19:46
+ */
+
+
+return [
+    'Time' => 'Tiempo',
+    'Name' => 'Nombre',
+    'Phone number' => 'Número de teléfono',
+    'Callback requests' => 'Solicitudes de llamada de vuelta',
+    'Search' => 'Buscar',
+    'Request deleted' => 'Solicitud eliminada',
+    'Callback request' => 'Solicitud de llamada de vuelta',
+    'Send' => 'Enviar',
+    'Close' => 'Cerrar',
+    'Thank you' => 'Gracias',
+    'Fill your name and phone number and we will callback you as soon as possible.' =>
+        'Complete su nombre y número de teléfono y le devolveremos la llamada lo antes posible.',
+    'We will contact you soon. This window will be closed in 5 sec.' => 'Nos pondremos en contacto con usted pronto. Esta ventana se cerrará automáticamente en 5 segundos.',
+    'Close now' => 'Cerrar ahora',
+    'New callback request from website:' => 'Nueva solicitud de llamada desde el sitio web:',
+    'New callback request' => 'Nueva solicitud de llamada',
+    'Topic' => 'Tema',
+    'All topics' => 'Todos los temas',
+];
